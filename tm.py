1f2c57d7671aaa9b73ebbb834fe0d3ad316ddc35
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-11-19 00:37:43 +0000

    Added methods to movement in tm

--- tm.py
+++ tm.py
@@ -1,22 +1,38 @@
 from reader import Reader
+import sys
+
 class Tm():
     def __init__(self, filename):
        reader = Reader(filename)
        self.Q, self.E, self.r, self.S, self.q0, self.input = reader.read_file()
-       self.actual = '{' + self.q0 + '}'
-       self.tape = self.actual + self.input
+       self.actual = self.q0
+       self.position = 0
+       self.refresh_tape() 
+
+    def refresh_tape(self):
+        self.tape = self.input[:self.position] + "{" + self.actual + "}" + self.input[self.position:len(self.input)]
 
     def compute(self):
         pass        
 
     def moveLeft(self):
-        pass
+        try: 
+            self.position -= 1
+            if self.position < 0:
+                raise Exception("Movimento inválido!")
+            self.refresh_tape() 
+        except Exception as error:
+            print('Erro encontrado: ' + repr(error)) 
+            sys.exit(1)
 
     def moveRight(self):
-        pass
+        self.position += 1
+        self.refresh_tape() 
 
     def __str__(self):
         return self.tape
 
 tm = Tm("entrada.txt")
+tm.moveRight()
+tm.moveLeft()
 print (tm)
