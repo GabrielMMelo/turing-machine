cc98fdb8ab590c475a238f8fa0aa04bb07f1dd02
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-11-21 23:52:49 +0000

    Implemented move_right special treatment and rename some variables

--- tm.py
+++ tm.py
@@ -6,26 +6,28 @@ class Tm():
        reader = Reader(filename)
        self.Q, self.E, self.r, self.S, self.q0, self.input = reader.read_file()
        self.transitions = {}
+       # Dicionario de dicionarios para armazenamento das transições de cada estado.
+       # { 'vertice_1': {'simbolo_leitura_1': ('proximo_estado', 'simbolo_escrita', 'direcao'), ...}, ...}
+       # {'q0': {'B': ('q1', '1', 'R'), ...}, 'q1': {'1', ...}, ... 'qn': { ...}} 
        for state in self.Q:
            self.transitions[state] = {}
-           for trans in self.S:
-               if trans[0] == state:
-                   self.transitions[state][trans[1]] = trans[2:len(trans)-1]
+           for source, in_symbol, destination, out_symbol, direction, _ in self.S:
+               if source == state:
+                   self.transitions[state][in_symbol] = [destination, out_symbol, direction]
        self.actual = self.q0
        self.position = 0
 
     # Funcao que printa o estado atual e a cabeca de leitura na fita
-    def refresh_tape(self):
-        print(self.input[:self.position] + "{" + self.actual + "}" + self.input[self.position:])
+    def show_tape(self):
+        print("".join([self.input[:self.position], "{", self.actual, "}", self.input[self.position:]]))
 
     # Funcao que executa a maquina de turing, movendo a cabeca de leitura 
     def compute(self):
-        # Lendo o próximo simbolo, vai para algum lugar?
-        self.refresh_tape()   
-        transition = self.get_transition()   
-        self.write_actual(transition[1])
-        self.actual = transition[0]
-        if transition[2] == 'R':
+        self.show_tape()   
+        destination, out_symbol, direction = self.get_transition()   
+        self.write_actual(out_symbol)
+        self.actual = destination
+        if direction == 'R':
             self.move_right()
         else:
             self.move_left()
@@ -47,6 +49,8 @@ class Tm():
     # Funcao que move a cabeca de leitura para direita
     def move_right(self):
         self.position += 1
+        if self.position == len(self.input):
+            self.input = "".join([self.input, 'B'])
 
     # Funcao que faz a escrita no local aonde esta a cabeca de leitura
     def write_actual(self, value):
@@ -58,6 +62,7 @@ class Tm():
     def read_actual(self):
         return self.input[self.position]
 
+# TODO: receber arquivo como argumento
 tm = Tm("entrada.txt")
 while True:
     try:
